26ae5afbd147cc2edad78f70a8a8826567ed4bed
Daniel <daniel@marquez.ai>
2020-05-11 19:52:38 -0400

    testing annotation

--- project/mask_rcnn.py
+++ project/mask_rcnn.py
@@ -9,7 +9,7 @@ import pickle
 
 img_rows, img_cols = 1200, 675
 Number_Images = len(listdir('data/images'))
-Fraction_Training_set = 0.9
+Fraction_Training_set = 0.99
 
 
 def data_frame_to_pickle(data_frame, annotations_dir):
@@ -155,23 +155,27 @@ class VesselConfig(Config):
 # load_test = HumanInVesselDangerDataset()
 # load_test.load_dataset('data/')
 
-# prepare train set
-train_set = HumanInVesselDangerDataset()
-train_set.load_dataset('data/', is_train=True)
-train_set.prepare()
-print('Train: %d' % len(train_set.image_ids))
-# prepare test/val set
 test_set = HumanInVesselDangerDataset()
 test_set.load_dataset('data/', is_train=False)
-test_set.prepare()
 print('Test: %d' % len(test_set.image_ids))
-# prepare config
-config = VesselConfig()
-config.display()
-# define the model
-model = MaskRCNN(mode='training', model_dir='./', config=config)
-# load weights (mscoco) and exclude the output layers
-model.load_weights('mask_rcnn_coco.h5', by_name=True,
-                   exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", "mrcnn_bbox", "mrcnn_mask"])
-# train weights (output layers or 'heads')
-model.train(train_set, test_set, learning_rate=config.LEARNING_RATE, epochs=5, layers='heads')
+
+# # prepare train set
+# train_set = HumanInVesselDangerDataset()
+# train_set.load_dataset('data/', is_train=True)
+# train_set.prepare()
+# print('Train: %d' % len(train_set.image_ids))
+# # prepare test/val set
+# test_set = HumanInVesselDangerDataset()
+# test_set.load_dataset('data/', is_train=False)
+# test_set.prepare()
+# print('Test: %d' % len(test_set.image_ids))
+# # prepare config
+# config = VesselConfig()
+# config.display()
+# # define the model
+# model = MaskRCNN(mode='training', model_dir='./', config=config)
+# # load weights (mscoco) and exclude the output layers
+# model.load_weights('mask_rcnn_coco.h5', by_name=True,
+#                    exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", "mrcnn_bbox", "mrcnn_mask"])
+# # train weights (output layers or 'heads')
+# model.train(train_set, test_set, learning_rate=config.LEARNING_RATE, epochs=5, layers='heads')
